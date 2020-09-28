5d816b58990321bf2d1309cccac0c9cf2e63c2b6
akashgupta3852 <akashgupta3852@gmail.com>
2020-09-28 22:29:17 +0530

    [Akas]Added: validation of abc part

--- src/com/validatingEmail/ValidatingEmail.java
+++ src/com/validatingEmail/ValidatingEmail.java
@@ -1,9 +1,27 @@
 package com.validatingEmail;
 
+import java.util.*;
+import java.util.regex.*;
+
 public class ValidatingEmail {
+	
+	public static String isValidEmailId(String emailId) {
+		String regex="^[a-zA-Z0-9]+(@bridgelabz.com)";
+		Pattern pattern=Pattern.compile(regex);
+		if(pattern.matcher(emailId).matches())
+			return "correct";
+		else
+			return "wrong";
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Email Valiadtion Program");
+
+			System.out.println("Enter the email id:");
+			Scanner scan=new Scanner(System.in);
+			String email=scan.nextLine();
+			
+			System.out.println("The email id: "+email+" is "+isValidEmailId(email));
 	}
 
 }
